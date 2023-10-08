b076b379c2556e57c12bd5e3b5396564ec66bc1e
Viktoriia <pecourses>
2023-10-08 18:10:53 +0300

    Add props

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,78 +13,53 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
 //  <></> - react fragment (для компонування кількох елементів)
-
-function App() {
-  return (
-    <>
-      <Header />
-      <main title="Hello">Hello React</main>
-      <UserInfo />
-      <NewsItem />
-      <Footer />
-    </>
-  );
-}
-
-function UserInfo() {
-  const user = {
-    name: 'Ivo',
-    age: 15,
-    imgSrc:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
-  };
-
-  const userNameStyle = {
-    color: user.age >= 18 ? 'blue' : 'green',
-  };
-
-  return (
-    <article className="userCard">
-      <h2 style={userNameStyle}>{user.name}</h2>
-      <img src={user.imgSrc} alt={user.name} />
-    </article>
-  );
-}
-
-// Реалізувати компонент (розмітка  + інлайн-стилі)
-const news = { title: 'Some news', body: 'Something hapenned', isGood: true };
-// гарна новина - зелена, не гарна - червона
-
-function NewsItem() {
-  const news = { title: 'Some news', body: 'Something hapanned', isGood: true };
-
-  const bodyColor = {
-    backgroundColor: news.isGood ? 'green' : 'red',
-  };
-
-  return (
-    <article className="newsInfo" style={bodyColor}>
-      <h2>{news.title}</h2>
-      <p>{news.body}</p>
-    </article>
-  );
-}
-
 // class => className = "class1 class2"
 // атрибути іменуються в camelCase
 // style - object
 //         імена властивослей - в camelCase
 //    backgroundColor: 'yellow',
 
-function Header() {
+function App() {
+  const isLight = false;
+  const name = 'Jack';
+  const user = { name: '', age: 20 };
   return (
-    <header className="header">
-      <h1>Page title</h1>
-    </header>
+    <>
+      <ThemeButton isLightTheme={isLight} />
+      <Greeting isGreeting={true} userName={name} />
+      <Greeting isGreeting={false} userName="Ivo" />
+    </>
   );
 }
 
-function Footer() {
+// ThemeButton({isLightTheme: isLight})
+// <ThemeButton isLightTheme={isLight}/>
+
+function ThemeButton(props) {
+  const { isLightTheme } = props;
+
+  const buttonStyle = {
+    color: isLightTheme ? 'black' : 'white',
+    backgroundColor: isLightTheme ? 'white' : 'black',
+  };
+
   return (
-    <footer>
-      <p>F&Q</p>
-      <p>About Us</p>
-      <p>Home</p>
-    </footer>
+    <button style={buttonStyle}>
+      Switch to {isLightTheme ? 'dark' : 'light'}
+    </button>
+  );
+}
+
+// Реалізувати компонент Greating, який буде виводити рядок "Hello, ІМʼЯ",
+// де ім'я задане в App
+// isGreeting={true} => "Hello, ІМʼЯ"
+// isGreeting={false} => "Goodbye, ІМʼЯ"
+
+function Greeting(props) {
+  const { userName, isGreeting } = props;
+  return (
+    <div>
+      {isGreeting ? 'Hello' : 'Goodbye'} {userName}
+    </div>
   );
 }
